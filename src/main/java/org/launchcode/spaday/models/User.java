ccbfc862791ded5d435f1b21262398d054af9b60
Nathan-422 <natiouswright@gmail.com>
2023-02-23 20:39:45 -0600

    Finished studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @Size(min = 5, max = 15, message = "Username must be 5-15 characters")
     private String username;
+
+    @Email(message = "Please try again")
     private String email;
+
+    @NotEmpty(message = "Password cannot be empty")
+    @Min(message = "Password must be at least 6 characters", value = 6)
     private String password;
 
     public User() {
